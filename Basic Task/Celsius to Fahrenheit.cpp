87b8a29c3c9332b43eea754c3b363cef8611785f
Yash Rohan <111037103+YashRohan07@users.noreply.github.com>
2022-10-07 21:05:15 +0600

    Add files via upload

---
+++ Basic Task/Celsius to Fahrenheit.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+
+int main(){
+
+double farn,cels;
+
+cout<<"Enter Fahrenheit:";
+cin>>farn;
+
+cels = (farn - 32) / 1.8;
+cout<<"Celsius is: "<<cels;
+
+
+getch();
+}
+
