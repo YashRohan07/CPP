f8f3e9c9ae9a671d5df1c324114e3473586d7767
Yash Rohan <111037103+YashRohan07@users.noreply.github.com>
2022-11-02 20:27:45 +0600

    Add files via upload

---
+++ Basic Task/Large-Small.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int num1,num2;
+
+    cout<<"Enter Two Number: ";
+    cin>>num1>>num2;
+
+    if(num1>num2)
+    {
+        cout<<"Number-1 is bigger";
+    }
+
+    else{
+        cout<<"Number-2 is bigger";
+    }
+}
---
+++ Basic Task/Pass-Fail.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int marks;
+
+    cout<<"Enter Your Marks: ";
+    cin>>marks;
+
+    if(marks>=33)
+    {
+        cout<<"Passed";
+    }
+
+    else{
+        cout<<"Failed";
+    }
+}
+
