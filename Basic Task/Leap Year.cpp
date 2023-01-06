3b9265716b4183c6d4e732c6d45fc6ec7c1ec799
Yash Rohan <111037103+YashRohan07@users.noreply.github.com>
2023-01-06 11:27:01 +0600

    Add files via upload

---
+++ Basic Task/Leap Year.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int year;
+    cout<<"Enter a Year: ";
+    cin>>year;
+
+    if(year%4 == 0  &&  year%100 != 0 )
+    {
+        cout<<"Leap Year";
+    }
+
+    else if(year%400 == 0)
+    {
+        cout<<"Leap Year";
+    }
+
+    else{
+        cout<<"Not Leap Year";
+    }
+
+}
