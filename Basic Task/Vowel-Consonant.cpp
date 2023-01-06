e99cf4525ac2e4be3d503418649c5c158e4bb466
Yash Rohan <111037103+YashRohan07@users.noreply.github.com>
2023-01-06 11:32:57 +0600

    Add files via upload

---
+++ Basic Task/Vowel-Consonant.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+   char ch;
+   cout<<"Enter Any Letter: ";
+   cin>>ch;
+
+   if(ch=='a' || ch=='e' || ch=='i' || ch=='o' || ch=='u')
+   {
+       cout<<"It Is Vowel!";
+   }
+
+   else{
+    cout<<"It Is COnsonant!";
+   }
+
+}
