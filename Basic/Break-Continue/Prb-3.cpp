741e0730c086e72017aa09825a612c30f5946e43
Yash Rohan <111037103+YashRohan07@users.noreply.github.com>
2023-01-06 10:18:16 +0600

    Add files via upload

---
+++ Basic/Break-Continue/Prb-3.cpp
@@ -0,0 +1,16 @@
+/* write a program to print all the numbers from 1 to 100, Except the number which are divisible by 3 */
+
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    for(int i=1; i<=100; i++){
+
+        if(i%3 == 0){
+            continue;
+        }
+        cout<<i<<endl;
+    }
+}
+
