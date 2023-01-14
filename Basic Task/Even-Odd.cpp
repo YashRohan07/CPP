a03386d61ddab3ad287ff1730fd9db79cb97993d
Yash Rohan <111037103+YashRohan07@users.noreply.github.com>
2023-01-14 10:00:26 +0600

    Update Even-Odd.cpp

--- Basic Task/Even-Odd.cpp
+++ Basic Task/Even-Odd.cpp
@@ -17,3 +17,22 @@ int main()
         cout<<"Odd";
     }
 }
+
+/* Using Conditional or Ternary Operator
+
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int num;
+    cout<<"Enter any integer: ";
+    cin>>num;
+
+    (num%2==0) ? cout<<num<<" Is Even"  : cout<<num<<" Is Odd";
+}
+
+// Expression-1 ? Expression-2 : Expression-3;   (If Expression-1 is true then Expression-2 will print, If Expression-1 is false then Expression-3 will print)
+
+
+*/
